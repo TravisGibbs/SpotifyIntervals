5df203d9d3c8a9de24109de9cef024e2de9240cf
Travis Gibbs <travisgibbs.2019@gmail.com>
2020-08-13 16:11:07 -0500

    Finished Generate View
    - Songs generating with multi artist selection
    - Clears xml after searching may add more xml elements so it doesnt look like it does :dizzy_face:

--- app/src/main/java/fragments/GenerateFragment.java
+++ app/src/main/java/fragments/GenerateFragment.java
@@ -1,6 +1,5 @@
 package fragments;
 
-import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -16,6 +15,7 @@ import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
 import com.example.spotifyintervals.R;
 import com.google.android.material.chip.Chip;
@@ -36,19 +36,27 @@ import services.SongService;
 public class GenerateFragment extends Fragment {
 
     private static final String TAG = "Generate";
-    HashMap<String, objectID> nameIdMap = new HashMap<String, objectID>();
+    HashMap<String, objectID> nameIdMapIntense = new HashMap<String, objectID>();
+    HashMap<String, objectID> nameIdMapRelax = new HashMap<String, objectID>();
     private String radioButtonSelected = "";
     private int count = 0;
     private ChipGroup searchResults;
+    private ChipGroup searchResults2;
     private Chip chip0;
     private Chip chip1;
     private Chip chip2;
     private Chip chip3;
     private Chip chip4;
+    private Chip chip5;
+    private Chip chip6;
+    private Chip chip7;
+    private Chip chip8;
+    private Chip chip9;
     private Button makePlaylistButton;
     private Button generateButton;
     private Button getSongsButton;
-    private Button searchButton;
+    private Button searchButtonIntense;
+    private Button searchButtonRelaxed;
     private RadioGroup radioGroup;
     private RadioButton radioTempo;
     private RadioButton radio352;
@@ -59,6 +67,7 @@ public class GenerateFragment extends Fragment {
     private Slider sliderDanceRelax;
     private Slider sliderEnergyRelax;
     private Slider sliderValenceRelax;
+    private ScrollView scrollView;
     private ArrayList<String> customIdArtists = new ArrayList<>();
     private ArrayList<String> customIdSongs = new ArrayList<>();
     private RelativeLayout relativeLayout;
@@ -77,14 +86,23 @@ public class GenerateFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         searchResults = view.findViewById(R.id.searchObjects);
+        searchResults2 = view.findViewById(R.id.searchObjects);
         chip0 = view.findViewById(R.id.chip0);
         chip1 = view.findViewById(R.id.chip1);
         chip2 = view.findViewById(R.id.chip2);
         chip3 = view.findViewById(R.id.chip3);
         chip4 = view.findViewById(R.id.chip4);
+        chip5 = view.findViewById(R.id.chip5);
+        chip6 = view.findViewById(R.id.chip6);
+        chip7 = view.findViewById(R.id.chip7);
+        chip8 = view.findViewById(R.id.chip8);
+        chip9 = view.findViewById(R.id.chip9);
+        scrollView = view.findViewById(R.id.scrollView);
         generateButton = view.findViewById(R.id.generateButton);
         makePlaylistButton = view.findViewById(R.id.makePlaylistButton);
-        searchButton = view.findViewById(R.id.searchButton);
+        searchButtonIntense = view.findViewById(R.id.searchButton);
+        searchButtonRelaxed = view.findViewById(R.id.searchButton2);
+        radioGroup = view.findViewById(R.id.radioGroup);
         radio352 = view.findViewById(R.id.radioAlternate);
         radioTempo = view.findViewById(R.id.radioTempo);
         sliderSets = view.findViewById(R.id.setSlider);
@@ -99,13 +117,24 @@ public class GenerateFragment extends Fragment {
         songService2 = new SongService(view.getContext(), relativeLayout);
         playlistService = new PlaylistService(view.getContext(), relativeLayout);
 
-        searchButton.setOnClickListener(new View.OnClickListener() {
+        searchButtonIntense.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (nameIdMap.size() >= 5) {
+                if (nameIdMapIntense.size() >= 5) {
                     Snackbar.make(relativeLayout, "Remove an artist and try again", Snackbar.LENGTH_SHORT).show();
                 } else {
-                    startSearchActivity();
+                    startSearchActivity(false);
+                }
+            }
+        });
+
+        searchButtonRelaxed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (nameIdMapRelax.size() >= 5) {
+                    Snackbar.make(relativeLayout, "Remove an artist and try again", Snackbar.LENGTH_SHORT).show();
+                } else {
+                    startSearchActivity(true);
                 }
             }
         });
@@ -127,35 +156,70 @@ public class GenerateFragment extends Fragment {
         chip0.setOnCloseIconClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                chipListenerResponse(chip0);
+                chipListenerResponse(chip0, 0);
             }
         });
 
         chip1.setOnCloseIconClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                chipListenerResponse(chip1);
+                chipListenerResponse(chip1, 1);
             }
         });
 
         chip2.setOnCloseIconClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                chipListenerResponse(chip2);
+                chipListenerResponse(chip2, 2);
             }
         });
 
         chip3.setOnCloseIconClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                chipListenerResponse(chip3);
+                chipListenerResponse(chip3, 3);
             }
         });
 
         chip4.setOnCloseIconClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                chipListenerResponse(chip4);
+                chipListenerResponse(chip4, 4);
+            }
+        });
+
+        chip5.setOnCloseIconClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chipListenerResponse(chip5, 5);
+            }
+        });
+
+        chip6.setOnCloseIconClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chipListenerResponse(chip6, 6);
+            }
+        });
+
+        chip7.setOnCloseIconClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chipListenerResponse(chip7, 7);
+            }
+        });
+
+        chip8.setOnCloseIconClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chipListenerResponse(chip8, 8);
+            }
+        });
+
+        chip9.setOnCloseIconClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chipListenerResponse(chip9, 9);
             }
         });
     }
@@ -215,38 +279,73 @@ public class GenerateFragment extends Fragment {
             if (resultCode == 0) {
                 return;
             }
-            setSearchResults(data);
+            setSearchResults(data, false);
+        }
+        if (requestCode == 3) {
+            if (requestCode == 0) {
+                return;
+            }
+            setSearchResults(data, true);
         }
     }
 
-    private void setSearchResults(Intent data) {
-        nameIdMap.put(data.getStringExtra(constants.objectNameKey),
-                new objectID(data.getBooleanExtra(constants.isSongKey, false), data.getStringExtra(constants.objectIDKey)));
-        if (chip0.getText().toString().equals("")) {
-            chip0.setText(data.getStringExtra(constants.objectNameKey));
-            chip0.setVisibility(View.VISIBLE);
-        } else if (chip1.getText().toString().equals("")) {
-            chip1.setText(data.getStringExtra(constants.objectNameKey));
-            chip1.setVisibility(View.VISIBLE);
-        } else if (chip2.getText().toString().equals("")) {
-            chip2.setText(data.getStringExtra(constants.objectNameKey));
-            chip2.setVisibility(View.VISIBLE);
-        } else if (chip3.getText().toString().equals("")) {
-            chip3.setText(data.getStringExtra(constants.objectNameKey));
-            chip3.setVisibility(View.VISIBLE);
-        } else if (chip4.getText().toString().equals("")) {
-            chip4.setText(data.getStringExtra(constants.objectNameKey));
-            chip4.setVisibility(View.VISIBLE);
+    private void setSearchResults(Intent data, boolean relaxed) {
+        if (!relaxed) {
+            nameIdMapIntense.put(data.getStringExtra(constants.objectNameKey),
+                    new objectID(data.getBooleanExtra(constants.isSongKey, false), data.getStringExtra(constants.objectIDKey)));
+            if (chip0.getText().toString().equals("")) {
+                chip0.setText(data.getStringExtra(constants.objectNameKey));
+                chip0.setVisibility(View.VISIBLE);
+            } else if (chip1.getText().toString().equals("")) {
+                chip1.setText(data.getStringExtra(constants.objectNameKey));
+                chip1.setVisibility(View.VISIBLE);
+            } else if (chip2.getText().toString().equals("")) {
+                chip2.setText(data.getStringExtra(constants.objectNameKey));
+                chip2.setVisibility(View.VISIBLE);
+            } else if (chip3.getText().toString().equals("")) {
+                chip3.setText(data.getStringExtra(constants.objectNameKey));
+                chip3.setVisibility(View.VISIBLE);
+            } else if (chip4.getText().toString().equals("")) {
+                chip4.setText(data.getStringExtra(constants.objectNameKey));
+                chip4.setVisibility(View.VISIBLE);
+            }
+        } else {
+            nameIdMapRelax.put(data.getStringExtra(constants.objectNameKey),
+                    new objectID(data.getBooleanExtra(constants.isSongKey, false), data.getStringExtra(constants.objectIDKey)));
+            if (chip5.getText().toString().equals("")) {
+                chip5.setText(data.getStringExtra(constants.objectNameKey));
+                chip5.setVisibility(View.VISIBLE);
+            } else if (chip6.getText().toString().equals("")) {
+                chip6.setText(data.getStringExtra(constants.objectNameKey));
+                chip6.setVisibility(View.VISIBLE);
+            } else if (chip7.getText().toString().equals("")) {
+                chip7.setText(data.getStringExtra(constants.objectNameKey));
+                chip7.setVisibility(View.VISIBLE);
+            } else if (chip8.getText().toString().equals("")) {
+                chip8.setText(data.getStringExtra(constants.objectNameKey));
+                chip8.setVisibility(View.VISIBLE);
+            } else if (chip9.getText().toString().equals("")) {
+                chip9.setText(data.getStringExtra(constants.objectNameKey));
+                chip9.setVisibility(View.VISIBLE);
+            }
         }
     }
 
-    private void startSearchActivity() {
+    private void startSearchActivity(boolean relaxed) {
         Intent intent = new Intent(getContext(), SearchActivity.class);
-        startActivityForResult(intent, 2);
+        if (relaxed) {
+            startActivityForResult(intent, 3);
+        } else {
+          startActivityForResult(intent, 2);
+        }
     }
 
-    private void chipListenerResponse(Chip chip) {
-        nameIdMap.remove(chip.getText().toString());
+    private void chipListenerResponse(Chip chip, int chipNum) {
+        if (chipNum < 5) {
+            nameIdMapIntense.remove(chip.getText().toString());
+        } else {
+            nameIdMapRelax.remove(chip.getText().toString());
+        }
         chip.setVisibility(View.GONE);
         chip.setText("");
     }
@@ -255,22 +354,22 @@ public class GenerateFragment extends Fragment {
         int minutesIntense;
         int minutesRelaxed;
         if (radioTempo.isActivated()) {
-            minutesIntense = Math.round(sliderSets.getValue() * 15);
+            minutesIntense = Math.round(sliderSets.getValue() * 15); // pattern with a 1.5 times error allowance
             minutesRelaxed = Math.round(sliderSets.getValue() * 8);
         } else {
-            minutesIntense = Math.round(sliderSets.getValue() * 11);
+            minutesIntense = Math.round(sliderSets.getValue() * 12);
             minutesRelaxed = Math.round(sliderSets.getValue() * 6);
         }
         int songsIntense = minutesIntense / 3;
         int songsRelaxed = minutesRelaxed / 3;
-        for (objectID objectID : nameIdMap.values()) {
+        for (objectID objectID : nameIdMapIntense.values()) {
             if (objectID.isSong()) {
                 customIdSongs.add(objectID.getId());
             } else {
                 customIdArtists.add(objectID.getId());
             }
         }
-        if (nameIdMap.size() * 100 > songsIntense || nameIdMap.size() * 100 > songsRelaxed) {
+        if (nameIdMapIntense.size() * 100 > songsIntense || nameIdMapIntense.size() * 100 > songsRelaxed) {
             Float danceValueIntense = sliderDanceIntense.getValue();
             Float energyValueIntense = sliderEnergyIntense.getValue();
             Float valenceValueIntense = sliderValenceIntense.getValue();
@@ -281,7 +380,7 @@ public class GenerateFragment extends Fragment {
                 @Override
                 public void onSearchFinish(boolean found) {
                     if (found && count != 0) {
-                        startButtonSwap(makePlaylistButton, generateButton);
+                        clearScreen();
                     } else if (found) {
                         count++;
                     } else {
@@ -289,12 +388,21 @@ public class GenerateFragment extends Fragment {
                     }
                 }
             });
+            customIdArtists.clear();
+            customIdSongs.clear();
+            for (objectID objectID : nameIdMapRelax.values()) {
+                if (objectID.isSong()) {
+                    customIdSongs.add(objectID.getId());
+                } else {
+                    customIdArtists.add(objectID.getId());
+                }
+            }
             songService2.getSeedTracks(customIdSongs, customIdArtists, songsIntense, danceValueRelax, energyValueRelax, valenceValueRelax,
             new SongService.songServiceCallback() {
                 @Override
                 public void onSearchFinish(boolean found) {
                     if (found && count != 0) {
-                        startButtonSwap(makePlaylistButton, generateButton);
+                        clearScreen();
                     } else if (found) {
                         count++;
                     } else {
@@ -305,8 +413,8 @@ public class GenerateFragment extends Fragment {
         }
     }
 
-    private void startButtonSwap(Button appearButton, Button goneButton) {
-        appearButton.setVisibility(View.VISIBLE);
-        goneButton.setVisibility(View.GONE);
+    private void clearScreen() {
+        scrollView.setVisibility(View.GONE);
+        makePlaylistButton.setVisibility(View.VISIBLE);
     }
 }
